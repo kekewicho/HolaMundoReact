e90dc4ec3c1c8bd5d19b126777c51f17675529ea
kekewicho <luisruben2997@gmail.com>
2024-09-28 16:29:45 -0600

    Carga inicial de archivos

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{ width:'100%', display:'flex', justifyContent:'center', alignContent:'center', flexDirection:'column' }}>
+        <h3>Hola Mundo desde react</h3>
+        <p>Por: Luis Ramos</p>
+      </div>
+    </>
+  )
+}
+
+export default App
